6f81b600ac3d4f5de1d22d6d7e8d067fe34bc05e
Aryan Naik <66840062+aryanNaik123@users.noreply.github.com>
2021-01-14 23:34:33 +0000

    dog pog

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import json
 import random 
 import time 
 import asyncio
+import aiohttp
+import urllib
 from keep_alive import keep_alive
 
 client = discord.Client()
@@ -16,7 +18,6 @@ starter_encouragements = [
   'Hang in there', 
   "Atleast you're not a bot like me"
 ]
-what_arr = ['what','why','are','how','when','where','is']
 social_arr = ['twitter.com','youtube.com']
 
 def get_quote(): 
@@ -24,6 +25,12 @@ def get_quote():
   json_data = json.loads(response.text)
   quote = json_data[0]['q'] + ' -' + json_data[0]['a']
   return quote
+
+def dog_api(): 
+  reponse = requests.get('https://dog.ceo/api/breeds/image/random')
+  url = reponse.json()['message']
+  urllib.request.urlretrieve(url, "sample.jpg")
+
 
 @client.event 
 async def on_ready(): 
@@ -33,9 +40,10 @@ async def on_ready():
 async def on_message(message): 
   if message.author == client.user: 
     return 
-
   msg = message.content
-
+  if message.content.startswith('$dog'):
+    dog = dog_api()
+    await message.channel.send(file=discord.File('sample.jpg'))
   if message.content.startswith('$inspire'): 
     quote = get_quote() 
     await message.channel.send(quote)
@@ -43,22 +51,10 @@ async def on_message(message):
     await message.channel.send(random.choice(starter_encouragements))
   if (str(msg) == 'lol'): 
     await message.channel.send('stop saying lol')
-  if any(word in msg for word in what_arr):
-    await message.channel.send('your mom')
-    time.sleep(0.5)
-    await message.channel.send('GOT EM')
   if message.content.startswith('shark pog'):
     await message.channel.send(file=discord.File('tenor.gif'))
   if any(word in msg for word in social_arr):
     await message.channel.send('dont care')
-  if message.content.startswith('$daily'):
-    await message.channel.send('@everyone how are yall doing')
-    time.sleep(300)
-    await message.channel.send('@everyone how are yall doing')
-    time.sleep(300)
-    await message.channel.send('@everyone how are yall doing')
-    time.sleep(300)
-    await message.channel.send('@everyone how are yall doing')
   if message.content.startswith('$thumb'):
       channel = message.channel
       await channel.send('Send me that 👍 reaction, mate')
